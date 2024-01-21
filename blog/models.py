d8f9191984b54a9ff41301efccfe3ee703f7a6cc
Botalov Serg <89090168690@mail.ru>
2024-01-21 22:30:18 +0500

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    full_text = models.TextField()
+    summary = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    pubdate = models.DateTimeField()
+    # slug #TODO
+    # is_published #TODO
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from django.test import TestCase
 from django.http import HttpRequest
 from django.urls import resolve
 from .views import home_page
+from .models import Article
 
 class HomePageTest(TestCase):
     
@@ -17,3 +19,37 @@ class HomePageTest(TestCase):
         self.assertIn('<title>Сайт Сергея Боталова</title>', html)
         self.assertIn('<h1>Serg Botalov</h1>', html)        
         self.assertTrue(html.endswith('</html>'))
+
+
+class ArticleModelTest(TestCase):
+
+    def test_article_model_save_and_retrive(self):
+        article1 = Article(
+            title='article 1',
+            full_text='full_text 1',
+            summary='summury 1',
+            category='category 1',
+            pubdate=datetime.now(),
+        )
+        article1.save()
+
+        article2 = Article(
+            title='article 2',
+            full_text='full_text 2',
+            summary='summury 2',
+            category='category 2',
+            pubdate=datetime.now(),
+        )
+        article2.save()
+
+        all_articles = Article.objects.all()
+
+        self.assertEqual(len(all_articles), 2)
+        self.assertEqual(
+            all_articles[0].title,
+            article1.title
+        )
+        self.assertEqual(
+            all_articles[1].title,
+            article2.title
+        )
